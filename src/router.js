357d94a5c74d44b8a76fc206949f45c70d3a312d
cody-malcolm <cody.malcolm@ontariotechu.net>
2021-04-21 19:57:47 -0400

    Update title on routing

--- src/router.js
+++ src/router.js
@@ -2,19 +2,64 @@ import Vue from "vue";
 import Router from "vue-router";
 
 import Welcome from './views/Welcome.vue';
-import Projects from './views/Projects.vue';
-import Skills from './views/Skills.vue';
-import About from './views/About.vue';
-import Contact from './views/Contact.vue';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
     routes: [
-        {path: '/', name: 'welcome', component: Welcome},
-        {path: '/projects', name: 'projects', component: Projects},
-        {path: '/skills', name: 'skills', component: Skills},
-        {path: '/about', name: 'about', component: About},
-        {path: '/contact', name: 'contact', component: Contact}
-    ]
+        {
+            path: '/',
+            name: 'welcome',
+            component: Welcome,
+            meta: {
+                title: 'Welcome'
+            }
+        },
+        {
+            path: '/projects',
+            name: 'projects',
+            component: () => import('./views/Projects.vue'),
+            meta: {
+                title: 'Projects'
+            }
+        },
+        {
+            path: '/skills',
+            name: 'skills',
+            component: () => import('./views/Skills.vue'),
+            meta: {
+                title: 'Skills'
+            }
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: () => import('./views/About.vue'),
+            meta: {
+                title: 'About'
+            }
+        },
+        {
+            path: '/contact',
+            name: 'contact',
+            component: () => import('./views/Contact.vue'),
+            meta: {
+                title: 'Contact'
+            }
+        }
+    ],
+    mode: 'history',
+    base: process.env.BASE_URL
 });
+
+router.beforeEach((to, from, next) => {
+    const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title);
+
+    if(nearestWithTitle) {
+        document.title = nearestWithTitle.meta.title;
+    }
+
+    next();
+});
+
+export default router;
--- src/router/index.js
+++ src/router/index.js
@@ -1,60 +1,60 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Welcome from '../views/Welcome.vue'
-
-Vue.use(VueRouter);
-
-const routes = [
-    {
-        path: '/',
-        name: 'Welcome',
-        component: Welcome,
-        meta: {
-            title: 'Welcome'
-        }
-    },
-    {
-        path: '/projects',
-        name: 'Projects',
-        component: () => import('../views/Projects.vue'),
-        meta: {
-            title: 'Projects'
-        }
-    },
-    {
-        path: '/skills',
-        name: 'Skills',
-        component: () => import('../views/Skills.vue'),
-        meta: {
-            title: 'Skills'
-        }
-    },
-    {
-        path: '/about',
-        name: 'About',
-        component: () => import('../views/About.vue'),
-        meta: {
-            title: 'About'
-        }
-    },
-    {
-        path: '/contact',
-        name: 'Contact',
-        component: () => import('../views/Contact.vue'),
-        meta: {
-            title: 'Contact'
-        }
-    }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-});
-
-router.beforeEach(route => {
-    document.title = route.meta.title;
-});
-
-export default router;
+// import Vue from 'vue'
+// import VueRouter from 'vue-router'
+// import Welcome from '../views/Welcome.vue'
+//
+// Vue.use(VueRouter);
+//
+// const routes = [
+//     {
+//         path: '/',
+//         name: 'welcome',
+//         component: Welcome,
+//         meta: {
+//             title: 'Welcome'
+//         }
+//     },
+//     {
+//         path: '/projects',
+//         name: 'projects',
+//         component: () => import('../views/Projects.vue'),
+//         meta: {
+//             title: 'Projects'
+//         }
+//     },
+//     {
+//         path: '/skills',
+//         name: 'skills',
+//         component: () => import('../views/Skills.vue'),
+//         meta: {
+//             title: 'Skills'
+//         }
+//     },
+//     {
+//         path: '/about',
+//         name: 'about',
+//         component: () => import('../views/About.vue'),
+//         meta: {
+//             title: 'About'
+//         }
+//     },
+//     {
+//         path: '/contact',
+//         name: 'contact',
+//         component: () => import('../views/Contact.vue'),
+//         meta: {
+//             title: 'Contact'
+//         }
+//     }
+// ]
+//
+// const router = new VueRouter({
+//   mode: 'history',
+//   base: process.env.BASE_URL,
+//   routes
+// });
+//
+// router.beforeEach(route => {
+//     document.title = route.meta.title;
+// });
+//
+// export default router;
