1e92b4a7795a9bf16f8a880acce96311f623276b
cody-malcolm <cody.malcolm@ontariotechu.net>
2021-04-21 05:51:42 -0400

    Update title based on route

--- src/router/index.js
+++ src/router/index.js
@@ -5,33 +5,52 @@ const routes = [
     {
         path: '/',
         name: 'Welcome',
-        component: Welcome
+        component: Welcome,
+        meta: {
+            title: 'Welcome'
+        }
     },
     {
         path: '/projects',
         name: 'Projects',
-        component: () => import('../views/Projects.vue')
+        component: () => import('../views/Projects.vue'),
+        meta: {
+            title: 'Projects'
+        }
     },
     {
         path: '/skills',
         name: 'Skills',
-        component: () => import('../views/Skills.vue')
+        component: () => import('../views/Skills.vue'),
+        meta: {
+            title: 'Skills'
+        }
     },
     {
         path: '/about',
         name: 'About',
-        component: () => import('../views/About.vue')
+        component: () => import('../views/About.vue'),
+        meta: {
+            title: 'About'
+        }
     },
     {
         path: '/contact',
         name: 'Contact',
-        component: () => import('../views/Contact.vue')
+        component: () => import('../views/Contact.vue'),
+        meta: {
+            title: 'Contact'
+        }
     }
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes: routes
+});
+
+router.beforeEach(route => {
+    document.title = route.meta.title;
 });
 
 export default router;
