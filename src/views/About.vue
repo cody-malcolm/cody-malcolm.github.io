05089c7167422953f959aa4b303c3ded0f808abe
cody-malcolm <cody.malcolm@ontariotechu.net>
2021-04-21 20:10:54 -0400

    Get carousel working, unstyled

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,60 +1,57 @@
 <template>
 <div class="outer-container">
     <div class="inner-container">
-        <vue-slick-carousel v-bind="settings">
-            <div><h3>1</h3></div>
-            <div><h3>2</h3></div>
-            <div><h3>3</h3></div>
-            <!-- <div class="section" id="my">
-                <p class="centered">
-                    My name is Cody Malcolm and I'm an undergraduate studying Computer
-                    Science with a minor in Mathematics at
-                    <a href="https://ontariotechu.ca/" target="_blank">Ontario Tech
-                    University</a>. Although I am only in my third year, I am
-                    <span class="em">super</span>-talented.
-                </p>
-                <p class="centered">
-                    It takes a lot of confidence to make a statement like that, but if
-                    you've already seen my <router-link to="/projects">project showcase
-                    </router-link> then I'm sure you'll agree the claim is justified.
-                </p>
-            </div>
-            <div class="section" id="clean">
-                <p class="centered">
-                    I pride myself on writing code that is clean, semantic, and
-                    well-documented both internally and externally. I always adhere to
-                    best practices, even though it is fun to write brilliant one-line
-                    methods on websites like
-                    <a href="https://www.codewars.com/users/cody-malcolm">Code Wars</a>.
-                </p>
-            </div>
-            <div class="section" id="robust">
-                <p class="centered">
-                    I always strive to make my programs as robust as possible,
-                    guarding against incorrect or malicious user input, edge cases,
-                    and other possible exceptions.
-                </p>
-            </div>
-            <div class="section" id="efficient">
-                <p class="centered">
-                    I chose a minor in Mathematics because it's also important to me
-                    that the software I write is efficient, logically sound, and behaves
-                    as expected. For this, I've taken classes in Statistics, Discrete
-                    Mathematics, Computational Science, Optimization, and soon
-                    Probability - with demonstrated success comparable to
-                    that of my programming classes.
-                </p>
-            </div>
-            <div class="section" id="designs">
-                <p class="centered">
-                    When designing an application, I make sure to properly utilize all
-                    <a href="/projects">object-oriented design principles</a> in order
-                    to produce a project that is modular, maintainable, and DRY. My
-                    <router-link to="/projects">project showcase</router-link> contains
-                    numerous examples of my deep understanding of these principles.
-                </p>
-            </div> -->
-        </vue-slick-carousel>
+      <VueSlickCarousel class="carousel" :arrows="true" :dots="true">
+          <div class="section" id="my">
+              <p class="centered">
+                  My name is Cody Malcolm and I'm an undergraduate studying Computer
+                  Science with a minor in Mathematics at
+                  <a href="https://ontariotechu.ca/" target="_blank">Ontario Tech
+                      University</a>. Although I am only in my third year, I am
+                      <span class="em">super</span>-talented.
+                  </p>
+                  <p class="centered">
+                      It takes a lot of confidence to make a statement like that, but if
+                      you've already seen my <router-link to="/projects">project showcase
+                      </router-link> then I'm sure you'll agree the claim is justified.
+                  </p>
+              </div>
+              <div class="section" id="clean">
+                  <p class="centered">
+                      I pride myself on writing code that is clean, semantic, and
+                      well-documented both internally and externally. I always adhere to
+                      best practices, even though it is fun to write brilliant one-line
+                      methods on websites like
+                      <a href="https://www.codewars.com/users/cody-malcolm">Code Wars</a>.
+                  </p>
+              </div>
+              <div class="section" id="robust">
+                  <p class="centered">
+                      I always strive to make my programs as robust as possible,
+                      guarding against incorrect or malicious user input, edge cases,
+                      and other possible exceptions.
+                  </p>
+              </div>
+              <div class="section" id="efficient">
+                  <p class="centered">
+                      I chose a minor in Mathematics because it's also important to me
+                      that the software I write is efficient, logically sound, and behaves
+                      as expected. For this, I've taken classes in Statistics, Discrete
+                      Mathematics, Computational Science, Optimization, and soon
+                      Probability - with demonstrated success comparable to
+                      that of my programming classes.
+                  </p>
+              </div>
+              <div class="section" id="designs">
+                  <p class="centered">
+                      When designing an application, I make sure to properly utilize all
+                      <a href="/projects">object-oriented design principles</a> in order
+                      to produce a project that is modular, maintainable, and DRY. My
+                      <router-link to="/projects">project showcase</router-link> contains
+                      numerous examples of my deep understanding of these principles.
+                  </p>
+              </div>
+      </VueSlickCarousel>
         <!-- <div class="controls">
             <span class="previous">Previous</span>
             <span class="my">My</span>
@@ -70,31 +67,40 @@
 
 <script>
 import VueSlickCarousel from 'vue-slick-carousel';
-
-const settings = () => ({
-  "dots": true,
-  "dotsClass": "slick-dots custom-dot-class",
-  "edgeFriction": 0.35,
-  "infinite": false,
-  "speed": 500,
-  "slidesToShow": 1,
-  "slidesToScroll": 1
-});
+import 'vue-slick-carousel/dist/vue-slick-carousel.css';
+// import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css';
+// const settings = () => ({
+//   "dots": true,
+//   "dotsClass": "slick-dots custom-dot-class",
+//   "edgeFriction": 0.35,
+//   "infinite": false,
+//   "speed": 500,
+//   "slidesToShow": 1,
+//   "slidesToScroll": 1
+// });
 
 export default {
     name: 'About',
     components: {
         VueSlickCarousel
-    },
-    data() {
-        return {
-            settings: settings
-        }
     }
+    // data() {
+    //     return {
+    //         settings: settings
+    //     }
+    // }
 }
 </script>
 
 <style scoped>
+.carousel {
+    width: 100vw;
+}
+
+.slick-slider {
+    width: 100%;
+}
+
 .em {
     font-style: italic;
 }
